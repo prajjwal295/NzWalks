932ffb5ffc7e5ea2db1942fd9015721a907369dc
Pj <mishraprajjwal295@gmail.com>
2025-03-27 11:04:51 +0530

    dbcontext created

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NzWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Mark" };
+
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ Data/NzWalksDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NzWalks.API.Model.Domain;
+namespace NzWalks.API.Data
+{
+    public class NzWalksDbContext : DbContext
+    {
+        public NzWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        // all of this property represent the database colllection 
+        // when we run migration , this will create the tables
+        // prop --> shortcut for creating property dbset
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
